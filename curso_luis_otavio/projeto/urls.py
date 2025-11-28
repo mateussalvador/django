76ed92101ade8acdb3a4a28d57ef671e37b976ec
Mateus Salvador <mateuslimasalvador@gmail.com>
2025-11-28 11:17:14 -0300

    adicionando templates

--- curso_luis_otavio/projeto/urls.py
+++ curso_luis_otavio/projeto/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-# from django.http import HttpResponse -> Pode ser removido, visto que quem o utiliza agora é o arquivo de views do app
-from recipes.views import home, contato, sobre
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre),
+    path('', include('recipes.urls')),
 ]
--- curso_luis_otavio/recipes/views.py
+++ curso_luis_otavio/recipes/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-    return HttpResponse('HOME')
-
-def contato(request):
-    return HttpResponse('CONTATO')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
+    return render(request, 'recipes/pages/home.html')
